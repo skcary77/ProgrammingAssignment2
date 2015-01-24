d5794a46e7434edd69aaffe15a2d46055b8060e8
skcary77 <skcary@gmail.com>
2015-01-23 23:46:04 -0500

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions work together to allow the user to create a matrix,
+## store the inverse of that matrix in the cache, and then retrieve the inverse from the cache
+## when necessary.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(iMatrix = matrix()) {
         inverse <- NULL
+        #calling makeCacheMatrix$set(y) allows you to change the matrix without excuting
+        #the entire function. It also clears the inverse from the cache
         set <- function(y) {
                 iMatrix <<- y
                 inverse <<- NULL
         }
-        get <- function() iMatrix
-        setinverse <-function(i) inverse <<- i
-        getinverse <-function() inverse
+        get <- function() iMatrix #returns the matrix
+        setinverse <-function(inv) inverse <<- inv #takes the inverse and stores to cache
+        getinverse <-function() inverse #returns the inverse
+        #returns a list of the functions described above
         list(set = set, get = get,
              setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated, it is retrieved from the cache
 
 cacheSolve <- function(iMatrix, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## pulls the inverse from makeCacheMartix
         solved <- iMatrix$getinverse()
+        ## if it is not null, then return it
         if(!is.null(solved)){
                 #message("getting cached data")
                 #return(solved)
                 return()
         }
+        #otherwise, comput the inverse, store it to the cache, then return it
         data <- iMatrix$get()
         solved <- solve(data)
         iMatrix$setinverse(solved)
